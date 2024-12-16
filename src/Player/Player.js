120a797e7e3bdae088c7c8ea5c766eee09a5e6cb
svntax <endless.answers@gmail.com>
2024-12-15 23:25:48 -0500

    Added coyote time and seamless room warping for player.
    Added coyote time for player jumps, and adjusted the player's hitbox size.
    Set up duplicate tiles and player sprites offscreen for seamless room warping effect.

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -1,6 +1,8 @@
 import * as LittleJS from "littlejsengine";
-const { EngineObject, vec2, keyWasPressed, keyIsDown, gamepadWasPressed, gamepadIsDown, gamepadStick,  isUsingGamepad,
+const { EngineObject, Timer, vec2, keyWasPressed, keyIsDown, gamepadWasPressed, gamepadStick,  isUsingGamepad,
     clamp, sign } = LittleJS;
+
+import { roomWidthInTiles } from "../gameLevel";
 
 class Player extends EngineObject {
     constructor(pos){
@@ -10,17 +12,23 @@ class Player extends EngineObject {
         this.moveSpeed = 0.0625;
         
         this.drawSize = vec2(2); // Tiles are 16x16 but the sprite is 32x32
+        this.size = vec2(1.5, 0.75);
         this.tileInfo = LittleJS.tile(1, 32, 1);
         this.color = LittleJS.WHITE;
         this.mirror = false;
         this.setCollision(true, false);
         this.onGround = false;
+        this.jumpTimer = new Timer();
+        this.isJumping = false;
     }
 
     render(){
         let bodyPos = this.pos;
         bodyPos = bodyPos.add(vec2(0,(this.drawSize.y - this.size.y) / 2));
         LittleJS.drawTile(bodyPos, this.drawSize, this.tileInfo, this.color, 0, this.mirror);
+        // Draw copies of the player on both sides off screen to make the warping effect seamless.
+        LittleJS.drawTile(bodyPos.add(vec2(-roomWidthInTiles, 0)), this.drawSize, this.tileInfo, this.color, 0, this.mirror);
+        LittleJS.drawTile(bodyPos.add(vec2(roomWidthInTiles, 0)), this.drawSize, this.tileInfo, this.color, 0, this.mirror);
     }
 
     update(){
@@ -36,8 +44,12 @@ class Player extends EngineObject {
             this.mirror = (this.moveInput.x < 0);
         }
 
-        if(this.jumpPressed && this.onGround){
-            this.velocity.y = 0.4375;
+        if(this.jumpPressed){
+            if(this.onGround || this.jumpTimer.active()){
+                this.velocity.y = 0.4375;
+                this.jumpTimer.unset();
+                this.isJumping = true;
+            }
         }
 
         // Floaty jump
@@ -59,6 +71,10 @@ class Player extends EngineObject {
             this.velocity.y = 0;
             this.pos.y = lowerBoundY + this.drawSize.y / 2;
         }
+
+        if(this.isJumping && this.velocity.y <= 0){
+            this.isJumping = false;
+        }
         
         // apply movement acceleration and clamp
         const maxCharacterSpeed = 0.125;
@@ -72,12 +88,29 @@ class Player extends EngineObject {
             this.velocity.x *= .5; // moving against velocity (stopping)
         }
 
-        const raycastHit = LittleJS.tileCollisionRaycast(this.pos, this.pos.add(vec2(0, -1)), this);
-        if(raycastHit){
+        // Check if on ground
+        const wasOnGround = this.onGround;
+        const groundRay = vec2(0, -1);
+        const raycastHit = LittleJS.tileCollisionRaycast(this.pos, this.pos.add(groundRay), this);
+        const raycastHitLeft = LittleJS.tileCollisionRaycast(this.pos.add(vec2(this.size.x/2, 0)), this.pos.add(groundRay), this);
+        const raycastHitRight = LittleJS.tileCollisionRaycast(this.pos.add(vec2(-this.size.x/2, 0)), this.pos.add(groundRay), this);
+        if(raycastHit || raycastHitLeft || raycastHitRight){
             this.onGround = true;
         }
         else{
             this.onGround = false;
+        }
+        // Coyote timer
+        if(!this.onGround && wasOnGround && !this.isJumping){
+            this.jumpTimer.set(.2);
+        }
+
+        // Seamless room warping effect
+        if(this.pos.x >= roomWidthInTiles){
+            this.pos.x -= roomWidthInTiles;
+        }
+        if(this.pos.x < 0){
+            this.pos.x += roomWidthInTiles;
         }
 
         super.update();
--- src/gameLevel.js
+++ src/gameLevel.js
@@ -3,16 +3,23 @@ const {vec2, TileLayer, TileLayerData} = LittleJS;
 import level_1 from "./assets/levels/level_1.json";
 const levelsData = [level_1];
 
+export let roomWidthInTiles = 16;
+
 export default function loadLevel(level=0){
     const tileMapData = levelsData[level];
     const levelSize = vec2(tileMapData.width, tileMapData.height);
     LittleJS.initTileCollision(levelSize);
+    roomWidthInTiles = levelSize.x;
 
     // set all level data tiles
     const layerCount = tileMapData.layers.length;
     for (let layer=layerCount; layer--;){
         const layerData = tileMapData.layers[layer].data;
         const tileLayer = new TileLayer(vec2(), levelSize, LittleJS.tile(0,tileMapData.tilewidth,0));
+        // Copies of the tilemap for the left and right sides
+        const edgeBleedAmount = 0.05;
+        const tileLayerLeft = new TileLayer(vec2(-tileMapData.width + edgeBleedAmount, 0), levelSize, LittleJS.tile(0,tileMapData.tilewidth,0));
+        const tileLayerRight = new TileLayer(vec2(tileMapData.width - edgeBleedAmount, 0), levelSize, LittleJS.tile(0,tileMapData.tilewidth,0));
 
         for (let x=levelSize.x; x--;) 
         for (let y=levelSize.y; y--;)
@@ -22,9 +29,13 @@ export default function loadLevel(level=0){
             if(tile > 0){
                 const data = new TileLayerData(tile-1);
                 tileLayer.setData(pos, data);
+                tileLayerLeft.setData(pos, data);
+                tileLayerRight.setData(pos, data);
                 LittleJS.setTileCollisionData(pos, 1);
             }
         }
         tileLayer.redraw();
+        tileLayerLeft.redraw();
+        tileLayerRight.redraw();
     }
 }
