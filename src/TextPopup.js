19214396492d7c8a69f953780de5d3ade4fe2787
svntax <endless.answers@gmail.com>
2024-12-19 04:11:35 -0500

    TextPopup color can now be set.

--- src/TextPopup.js
+++ src/TextPopup.js
@@ -13,14 +13,15 @@ class TextPopup extends EngineObject {
         this.duration = 1.25;
         this.lifeTimer.set(this.duration);
         this.gravityScale = 0;
+        this.color = LittleJS.WHITE;
         this.setCollision(false, false, false, false);
     }
 
     render(){
         const textPos = this.pos.add(this.offset);
-        LittleJS.drawText(this.text, textPos, 0.5, LittleJS.WHITE, 0, LittleJS.WHITE, "center", "PressStart2P");
-        LittleJS.drawText(this.text, textPos.add(vec2(-roomWidthInTiles, 0)), 0.5, LittleJS.WHITE, 0, LittleJS.WHITE, "center", "PressStart2P");
-        LittleJS.drawText(this.text, textPos.add(vec2(roomWidthInTiles, 0)), 0.5, LittleJS.WHITE, 0, LittleJS.WHITE, "center", "PressStart2P");
+        LittleJS.drawText(this.text, textPos, 0.5, this.color, 0, LittleJS.WHITE, "center", "PressStart2P");
+        LittleJS.drawText(this.text, textPos.add(vec2(-roomWidthInTiles, 0)), 0.5, this.color, 0, LittleJS.WHITE, "center", "PressStart2P");
+        LittleJS.drawText(this.text, textPos.add(vec2(roomWidthInTiles, 0)), 0.5, this.color, 0, LittleJS.WHITE, "center", "PressStart2P");
     }
 
     update(){
